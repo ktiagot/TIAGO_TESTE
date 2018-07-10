9a8f84f1424601b397ba4a355facfa5675c4e587
ktiagot <tiagocalutis@hotmail.com.br>
2018-07-10 17:40:03 -0300

    Configuração da conexão com o banco de dados

---
+++ php/configuracao.php
@@ -0,0 +1,19 @@
+<?php
+	//Configurando conexão
+	$host = "localhost";
+	$user = "root";
+	$password = "";
+	$db = "taximanager";
+	$con = new mysqli($host, $user, $password, $db);
+
+	//Querys para o <select> da corrida contendo os passageiros e motoristas cadastrados
+	$query_passageiro = mysqli_query($con, "SELECT * FROM PASSAGEIROS");
+	$query_motorista = mysqli_query($con, "SELECT * FROM MOTORISTAS WHERE atividade='1'");
+	
+	//Querys para consulta
+	$query_consulta_motorista = "SELECT * FROM MOTORISTAS";
+	$query_consulta_passageiro = "SELECT * FROM PASSAGEIROS";
+	$query_consulta_corrida = "SELECT *	FROM CORRIDAS 
+	INNER JOIN MOTORISTAS ON CORRIDAS.MOTORISTA=MOTORISTAS.CPF
+	INNER JOIN PASSAGEIROS ON CORRIDAS.PASSAGEIRO=PASSAGEIROS.CPF;";
+?>
